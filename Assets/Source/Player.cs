88e419a20f90212880cb46f438b9b52ea9ba3145
Megawats777 <jedsemilla@gmail.com>
2017-05-05 00:02:15 -0400

    Added ability to enable, disable, and respawn the player

--- Assets/Source/Player.cs
+++ Assets/Source/Player.cs
@@ -26,50 +26,63 @@ public class Player : MonoBehaviour
     [SerializeField]
     private bool isRotating = true;
 
+    // The respawn location of the player
+    [HideInInspector]
+    public Vector3 respawnLocation;
+
+    // Is input enabled
+    private bool isInputEnabled = true;
+
     // Component references
     private Rigidbody rb;
+    private Renderer meshRenderer;
 
     // Called before start
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        meshRenderer = GetComponent<Renderer>();
     }
 
     // Use this for initialization
     void Start ()
     {
-		
+        respawnLocation = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {  
-		// If the spacebar is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
+        // If input is enabled
+        if (isInputEnabled == true)
         {
-            // If the player is not rotating
-            // Set the player to be rotating
-            if (isRotating == false)
+            // If the spacebar is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                isRotating = true; 
+                // If the player is not rotating
+                // Set the player to be rotating
+                if (isRotating == false)
+                {
+                    isRotating = true;
+                }
+
+                // If the player is rotating
+                // Set the player to not be rotating
+                else
+                {
+                    isRotating = false;
+                }
             }
 
-            // If the player is rotating
-            // Set the player to not be rotating
-            else
+            // If the W key is pressed
+            else if (Input.GetKeyDown(KeyCode.W))
             {
-                isRotating = false;
-            }
-        }
-
-        // If the W key is pressed
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            // If the player is rotating
-            // Flip the current rotation direction
-            if (isRotating == true)
-            {
-                flipRotationDirection();
+                // If the player is rotating
+                // Flip the current rotation direction
+                if (isRotating == true)
+                {
+                    flipRotationDirection();
+                }
             }
         }
     }
@@ -79,6 +92,17 @@ public class Player : MonoBehaviour
     {
         // Control player movement state
         controlPlayerMovementState();
+    }
+
+    // When this object collides with something
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If the colliding object is a wall
+        if (collision.transform.CompareTag("Wall"))
+        {
+            // Respawn the player
+            StartCoroutine(respawnPlayer());
+        }
     }
 
     // Control player movement state
@@ -95,7 +119,7 @@ public class Player : MonoBehaviour
         // If the player is rotating
         else
         {
-            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
             float usedRotationSpeed = 0.0f;
 
             // If the shift key is pressed
@@ -148,6 +172,46 @@ public class Player : MonoBehaviour
             print("Current rotation direction: " + currentRotationDirection.ToString());
         }
             
+    }
+
+    // Disable the player
+    public void disablePlayer()
+    {
+        // Disable the mesh renderer
+        // Start rotating the player
+        // Disable player input
+        // Disable any child objects
+        meshRenderer.enabled = false;
+        isInputEnabled = false;
+        isRotating = true;
+    }
+
+    // Enable the player
+    public void enablePlayer()
+    {
+        // Enable the mesh renderer
+        // Start rotating the player
+        // Enable player input
+        // Enable any child objects
+        meshRenderer.enabled = true;
+        isInputEnabled = true;
+        isRotating = true;
+    }
+
+    // Respawn the player
+    public IEnumerator respawnPlayer()
+    {
+        // Disable the player
+        disablePlayer();
+
+        // Have a delay
+        yield return new WaitForSeconds(1.0f);
+
+        // Set the location of the player to be the respawn location
+        transform.position = respawnLocation;
+
+        // Enable the player
+        enablePlayer();
     }
 }
 
