ad31b4f7f06559404600c4769639836a2f0f79c5
Megawats777 <jedsemilla@gmail.com>
2017-05-19 07:12:18 -0400

    Started to link level data to stage select buttons

---
+++ Assets/Source/Main Menu Classes/StageButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageButton : MonoBehaviour
+{
+    // The scene this stage is attached to
+    public string attatchedScene = string.Empty;
+
+    // HUD Objects
+    [SerializeField]
+    private Text highScoreText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Set the content of the high score text object
+        highScoreText.text = SaveGameManager.loadLevelHighScore(attatchedScene).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
