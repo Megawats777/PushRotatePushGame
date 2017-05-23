cb04e4c10eee2775df3e1982456f4553480361df
Megawats777 <jedsemilla@gmail.com>
2017-05-22 20:03:01 -0400

    Added help screen to main menu

---
+++ Assets/Source/Main Menu Classes/HelpHUD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpHUD : GameHUDBase
+{
+
+    public override void hideHUD()
+    {
+        hudContentRoot.SetActive(false);
+
+    }
+
+    public override void showHUD()
+    {
+        hudContentRoot.SetActive(true);
+
+    }
+
+    public override void updateHUDContent()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        hudContentRoot.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
