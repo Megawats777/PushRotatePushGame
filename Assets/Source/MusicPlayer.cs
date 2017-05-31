b977763b44ae67c3a9ae98ab6b4d722d6c7ea409
Megawats777 <jedsemilla@gmail.com>
2017-05-30 21:35:14 -0400

    Added music to project and finished music player

--- Assets/Source/MusicPlayer.cs
+++ Assets/Source/MusicPlayer.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    // A list of songs to play
+    [SerializeField]
+    private AudioClip[] songList;
 
-	// Use this for initialization
-	void Start ()
+    // Reference to the audio source component
+    private AudioSource audioSource;
+
+    // Called before start
+    private void Awake()
     {
-		
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Play a song
+        playSong();
 	}
 	
 	// Update is called once per frame
@@ -16,4 +30,16 @@ public class MusicPlayer : MonoBehaviour
     {
 		
 	}
+
+    // Play a song
+    private void playSong()
+    {
+        // Select a song to play
+        AudioClip chosenSong = songList[Random.Range(0, songList.Length - 1)];
+
+        // Play the song
+        audioSource.clip = chosenSong;
+        audioSource.Play();
+    }
+
 }
