0911751acbb7c4fad04bd0a7a68fc4e40bb1524a
Megawats777 <jedsemilla@gmail.com>
2017-05-31 20:54:12 -0400

    Added flash image animation in level transitions

---
+++ Assets/Source/LoadingScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Load the welcome screen
+        SceneManager.LoadScene("Scene_MainMenu_01");
+	}
+	
+	
+}
