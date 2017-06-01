31a616d6ea20448fcb4dbb2d8fbd6ce65876cef9
Megawats777 <jedsemilla@gmail.com>
2017-06-01 17:13:06 -0400

    Increased state transition speed for the flash image

---
+++ Assets/Source/HUD Classes/GameHUDRoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHUDRoot : MonoBehaviour
+{
+    // Reference to the flash image game object
+    [SerializeField]
+    private GameObject flashImageGameObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Enabled the flash image game object
+        flashImageGameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
